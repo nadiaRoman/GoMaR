9857269a1fac930fca568f960f4e5ee9a4565169
nadiaRoman <48381078+nadiaRoman@users.noreply.github.com>
2020-05-14 12:12:26 +0300

    Create login.js

---
+++ js/login.js
@@ -0,0 +1,80 @@
+function loginUser() {
+    const username = document.getElementById("username");
+    const password = document.getElementById("password");
+
+    validateAll();
+
+    if (username.classList.contains("invalid") ||
+        password.classList.contains("invalid")) {
+        return;
+    }
+
+    const data = { username: username.value, password: password.value };
+
+    fetch('http://127.0.0.1:3333/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data),
+    })
+        .then((response) => {
+            console.log(response);
+            response.json().then(function (data) {
+                if (response.status == 200) {
+                    console.log(data);
+                    if (data.authToken) {
+                        localStorage.setItem("authToken", data.authToken);
+                        window.location.replace("http://localhost:3000/");
+                    }
+                    console.log(response.status);
+                } else {
+                    displayToastr('fail', data.response);
+                }
+            });
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+}
+
+var body = document.getElementsByTagName("body")[0];
+
+body.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        loginUser();
+    }
+});
+
+function validateAll() {
+    validateUsernameField();
+    validatePasswordField();
+}
+
+
+function validateUsernameField() {
+    const username = document.getElementById("username");
+
+    if (!username.value) {
+        username.classList.add("invalid");
+    } else {
+        username.classList.remove("invalid");
+    }
+}
+
+function validatePasswordField() {
+    const password = document.getElementById("password");
+
+    if (!password.value) {
+        password.classList.add("invalid");
+    } else {
+        password.classList.remove("invalid");
+    }
+}
+
+document.getElementById("username").addEventListener("keyup", function (event) {
+    validateUsernameField();
+});
+document.getElementById("password").addEventListener("keyup", function (event) {
+    validatePasswordField();
+});
