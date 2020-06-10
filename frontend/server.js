e74e4a24ad9a0decf4b96e052c81d0fd8812be3e
raluca-dascalu <raluca.florea1@gmail.com>
2020-06-11 02:37:58 +0300

    frontend

--- frontend/server.js
+++ frontend/server.js
@@ -10,26 +10,40 @@ http.createServer(function (req, res) {
       fs.createReadStream(path.resolve(__dirname, url.substr(1))).pipe(res);
    } else {
       if (url.endsWith('.js')) {
-         console.log(url);
          res.writeHead(200, { 'Content-Type': 'text/javascript' });
          fs.createReadStream(path.resolve(__dirname, url.substr(1))).pipe(res);
       } else {
          if (url.includes('html-blocks')) {
-            console.log(url);
             fs.createReadStream(path.resolve(__dirname, url.substr(1))).pipe(res);
-         }
-         else {
+         } else {
             switch (url) {
                case '/':
+               case '/home':
+                  fs.createReadStream(path.resolve(__dirname, 'html/home.html')).pipe(res);
+                  break;
                case '/login':
                   fs.createReadStream(path.resolve(__dirname, 'html/login.html')).pipe(res);
                   break;
                case '/register':
                   fs.createReadStream(path.resolve(__dirname, 'html/register.html')).pipe(res);
                   break;
+               case '/configureProfile':
+                  fs.createReadStream(path.resolve(__dirname, 'html/configureProfile.html')).pipe(res);
+                  break;
+               case '/evaluation':
+                  fs.createReadStream(path.resolve(__dirname, 'html/evaluation.html')).pipe(res);
+                  break;
+               case '/highscores':
+                  fs.createReadStream(path.resolve(__dirname, 'html/highscores.html')).pipe(res);
+                  break;
                case '/recomandations':
                   fs.createReadStream(path.resolve(__dirname, 'html/recomandations.html')).pipe(res);
                   break;
+               case '/rss':
+                  treatRss(res);
+                  break;
+               default:
+                  fs.createReadStream(path.resolve(__dirname, 'html/404.html')).pipe(res);
             }
          }
       }
@@ -37,3 +51,20 @@ http.createServer(function (req, res) {
 }).listen(3000, function () {
    console.log("server start at port 3000");
 });
+
+function treatRss(res) {
+   res.writeHead(200, { 'Content-Type': 'application/xml' });
+   const options = {
+      hostname: 'localhost',
+      port: 3333,
+      path: '/rss',
+      method: 'GET'
+   }
+   const req = http.request(options, r => {
+      r.pipe(res);
+   })
+   req.on('error', error => {
+      console.error(error)
+   })
+   req.end()
+}
